2bd1f7a3541e776712a99fb26abb8ac5d7204dc3
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2021-01-10 18:26:28 +0100

    Upgrade to Gradle 6.8.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,9 +8,9 @@ repositories {
 }
 
 dependencies {
-    // Kotlin version embedded by Gradle 6.8-rc-4 is 1.4.20. We must not specify
-    // a different version in order to avoid conflicts with the embedded
-    // version. See https://youtrack.jetbrains.com/issue/KT-41142.
+    // Kotlin version embedded by Gradle 6.8 is 1.4.20. We must not specify a
+    // different version in order to avoid conflicts with the embedded version.
+    // See https://youtrack.jetbrains.com/issue/KT-41142.
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin")
     implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.4.20")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:5.8.2")
