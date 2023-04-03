9b29d2bc35c2b38385367a2b51a1567397428a52
Andrei Heidelbacher <andrei.heidelbacher@gmail.com>
2023-04-03 23:00:27 +0200

    Add chronolens-test to code coverage reports.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,34 +1,38 @@
 plugins {
-    base
-    id("test-report-aggregation")
-    id("jacoco-report-aggregation")
+  base
+  id("test-report-aggregation")
+  id("jacoco-report-aggregation")
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 tasks.wrapper {
-    distributionType = Wrapper.DistributionType.ALL
+  distributionType = Wrapper.DistributionType.ALL
 }
 
 dependencies {
-    // Direct and transitive project dependencies are selected for aggregation. The
-    // ":api:chronolens-cli" application project should assemble all other subprojects
-    // (":chronolens-model", ":chronolens-api", ":chronolens-core", and all ":plugins" subprojects).
-    testReportAggregation(project(":app:chronolens-cli"))
-    jacocoAggregation(project(":app:chronolens-cli"))
+  // Direct and transitive project dependencies are selected for aggregation. The
+  // ":api:chronolens-cli" application project should assemble all other subprojects
+  // (":chronolens-model", ":chronolens-api", ":chronolens-core", and all ":plugins" subprojects).
+  testReportAggregation(project(":app:chronolens-cli"))
+  jacocoAggregation(project(":app:chronolens-cli"))
+
+  // Also add the ":chronolens-test" subproject, as test utilities should have adequate coverage.
+  testReportAggregation(project(":chronolens-test"))
+  jacocoAggregation(project(":chronolens-test"))
 }
 
 reporting {
-    reports {
-        val aggregateTestReport by creating(AggregateTestReport::class) {
-            testType.set(TestSuiteType.UNIT_TEST)
-        }
-
-        val codeCoverageReport by creating(JacocoCoverageReport::class) {
-            testType.set(TestSuiteType.UNIT_TEST)
-        }
+  reports {
+    val aggregateTestReport by creating(AggregateTestReport::class) {
+      testType.set(TestSuiteType.UNIT_TEST)
     }
+
+    val codeCoverageReport by creating(JacocoCoverageReport::class) {
+      testType.set(TestSuiteType.UNIT_TEST)
+    }
+  }
 }
 
